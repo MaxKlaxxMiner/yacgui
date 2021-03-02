de3d60d4eaace1985ddbc780f3795ab56f811fdc
MaxKlaxxMiner <max@tetrisk.de>
2021-03-02 21:00:31 +0100

    v0.001: create visual studio project + add chess-pieces.png (from wikipedia: https://commons.wikimedia.org/wiki/Template:SVG_chess_pieces)

---
+++ MainForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace YacGui
+{
+  sealed partial class MainForm
+  {
+    /// <summary>
+    /// Erforderliche Designervariable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Verwendete Ressourcen bereinigen.
+    /// </summary>
+    /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Vom Windows Form-Designer generierter Code
+
+    /// <summary>
+    /// Erforderliche Methode für die Designerunterstützung.
+    /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.pictureBoxMain = new System.Windows.Forms.PictureBox();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // pictureBox1
+      // 
+      this.pictureBoxMain.Location = new System.Drawing.Point(12, 12);
+      this.pictureBoxMain.Name = "pictureBoxMain";
+      this.pictureBoxMain.Size = new System.Drawing.Size(1350, 450);
+      this.pictureBoxMain.TabIndex = 0;
+      this.pictureBoxMain.TabStop = false;
+      // 
+      // MainForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1373, 474);
+      this.Controls.Add(this.pictureBoxMain);
+      this.Name = "MainForm";
+      this.Text = "yacgui";
+      this.Load += new System.EventHandler(this.MainForm_Load);
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).EndInit();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.PictureBox pictureBoxMain;
+  }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,45 @@
+﻿#region # using *.*
+// ReSharper disable RedundantUsingDirective
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+#endregion
+
+namespace YacGui
+{
+  public sealed partial class MainForm : Form
+  {
+    /// <summary>
+    /// main-version: 0 == alpha, 1 >= release
+    /// </summary>
+    const int MainVersion = 0;
+    /// <summary>
+    /// sub-version
+    /// </summary>
+    const int SubVersion = 1;
+
+    /// <summary>
+    /// constructor
+    /// </summary>
+    public MainForm()
+    {
+      InitializeComponent();
+
+      Text = "YacGui - v" + MainVersion + "." + SubVersion.ToString("D3");
+    }
+
+    /// <summary>
+    /// load form
+    /// </summary>
+    void MainForm_Load(object sender, EventArgs e)
+    {
+      pictureBoxMain.Image = Properties.Resources.ChessPieces;
+    }
+  }
+}
