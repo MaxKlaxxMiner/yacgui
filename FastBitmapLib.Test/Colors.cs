fc067c351465721b0bacdbcfaf493a47271cca52
MaxKlaxxMiner <max@tetrisk.de>
2021-04-02 17:10:15 +0200

    v0.030: create unit test project

---
+++ FastBitmapLib.Test/Colors.cs
@@ -0,0 +1,38 @@
+﻿#region # using *.*
+// ReSharper disable RedundantUsingDirective
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using FastBitmapLib.Extras;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+// ReSharper disable MemberCanBePrivate.Global
+#endregion
+
+namespace FastBitmapLib.Test
+{
+  [TestClass]
+  public class Colors
+  {
+    public static Color[] GetTestColors(bool basics = true)
+    {
+      var result = new List<Color>();
+
+      if (basics)
+      {
+        foreach (var known in (KnownColor[])Enum.GetValues(typeof(KnownColor)))
+        {
+          result.Add(Color.FromKnownColor(known));
+        }
+      }
+
+      return result.ToArray();
+    }
+
+    [TestMethod]
+    public void TestMethod1()
+    {
+      var testColors = GetTestColors();
+      Assert.AreEqual(testColors.Length, 174);
+    }
+  }
+}
